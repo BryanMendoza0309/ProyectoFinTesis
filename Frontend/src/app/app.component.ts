8b46285f76e9835acc92b02d53b508d3d30fe69f
BryanMendoza0309 <andyloor0309@gmail.com>
2022-02-22 01:05:42 -0500

    Submenu

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -6,13 +6,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
+    {
+      title: 'Inicio',
+      url: 'folder/inbox',
+      icon: 'home'
+    },
+    {
+      title: 'SOMOS RC5',
+      children:[
+        {
+          title: 'Rafael Correa',
+          url: 'folder/inbox',
+          icon: 'home'
+        },
+        {
+          title: 'Sedes',
+          url: 'folder/inbox',
+          icon: 'home'
+        }
+      ]
+    }
+  ];
+  /*public appPages = [
     { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
     { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
+  ];*/
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
